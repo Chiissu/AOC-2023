f36f0fc5febb769b169c976c72f13e5a71b74539
froxcey <danichen204@gmail.com>
2023-12-03 17:25:27 +0800

    Clean up d2p2

--- day1-p2/src/main.zig
+++ day1-p2/src/main.zig
@@ -8,17 +8,8 @@ pub fn main() !void {
     var buf_reader = std.io.bufferedReader(input.reader());
     var in_stream = buf_reader.reader();
 
-    var numNames: [10][]const u8 = undefined;
-    numNames[0] = "zero";
-    numNames[1] = "one";
-    numNames[2] = "two";
-    numNames[3] = "three";
-    numNames[4] = "four";
-    numNames[5] = "five";
-    numNames[6] = "six";
-    numNames[7] = "seven";
-    numNames[8] = "eight";
-    numNames[9] = "nine";
+    const numNames: [10][]const u8 = .{ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    _ = numNames;
     var result: u64 = 0;
 
     // Iterate through each line
@@ -34,56 +25,21 @@ pub fn main() !void {
             var num: u8 = 0;
 
             if (57 < char) {
-                // Process character
-                var checkNum: u8 = 0;
-                switch (char) {
-                    101 => checkNum = 8,
-                    102 => {
-                        // F, might be 4 or 5
-                        if (line.len < i + 4) break :charCalc;
-                        switch (line[i + 1]) {
-                            111 => checkNum = 4,
-                            105 => checkNum = 5,
-                            else => break :charCalc,
-                        }
-                    },
-                    110 => checkNum = 9,
-                    111 => checkNum = 1,
-                    115 => {
-                        // S, might be 6 or 7
-                        if (line.len < i + 3) break :charCalc;
-                        switch (line[i + 1]) {
-                            105 => checkNum = 6,
-                            101 => checkNum = 7,
-                            else => break :charCalc,
-                        }
-                    },
-                    116 => {
-                        if (line.len < i + 3) break :charCalc;
-                        switch (line[i + 1]) {
-                            119 => checkNum = 2,
-                            104 => checkNum = 3,
-                            else => break :charCalc,
-                        }
-                    },
-                    122 => checkNum = 0,
-                    else => break :charCalc,
-                }
+                // The current character is a letter
+                const numCheckRes = checkForNum(line, i);
 
-                const checkNumStr: []const u8 = numNames[checkNum];
-                if (line.len < i + checkNumStr.len) break :charCalc;
+                // The current position does not have a number
+                if (numCheckRes == null) break :charCalc;
 
-                const slice = line[i .. i + checkNumStr.len];
-                if (!std.mem.eql(u8, slice, checkNumStr)) break :charCalc;
-
-                num = checkNum;
+                // Save the result
+                num = numCheckRes.?;
             } else {
-                // Process number
+                // The current character is a number, save the result
                 num = @truncate(char);
                 num = num - 48;
             }
 
-            // store the result
+            // Store the result
             if (firstNum == null) {
                 firstNum = num;
             }
@@ -91,10 +47,35 @@ pub fn main() !void {
         }
 
         std.debug.print("Number: {d}{d}\n", .{ firstNum.?, lastNum });
+
         // Add the result up
         result = result + firstNum.? * 10 + lastNum;
     }
 
     // Show result
     std.debug.print("Final answer: {d}\n", .{result});
+}
+
+fn checkForNum(line: []u8, position: u64) ?u8 {
+    const numNames: [10][]const u8 = .{ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    if (line.len > position + 4) {
+        const slice = line[position .. position + 5];
+        if (std.mem.eql(u8, slice, numNames[3])) return 3;
+        if (std.mem.eql(u8, slice, numNames[7])) return 7;
+        if (std.mem.eql(u8, slice, numNames[8])) return 8;
+    }
+    if (line.len > position + 3) {
+        const slice = line[position .. position + 4];
+        if (std.mem.eql(u8, slice, numNames[0])) return 0;
+        if (std.mem.eql(u8, slice, numNames[4])) return 4;
+        if (std.mem.eql(u8, slice, numNames[5])) return 5;
+        if (std.mem.eql(u8, slice, numNames[9])) return 9;
+    }
+    if (line.len > position + 2) {
+        const slice = line[position .. position + 3];
+        if (std.mem.eql(u8, slice, numNames[1])) return 1;
+        if (std.mem.eql(u8, slice, numNames[2])) return 2;
+        if (std.mem.eql(u8, slice, numNames[6])) return 6;
+    }
+    return null;
 }
